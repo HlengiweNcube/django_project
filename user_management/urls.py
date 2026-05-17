d5f4c23233b3e4dacef6f03b13245e255857c85c
HlengiweNcube <hlehlencube123@gmail.com>
2026-05-17 22:10:53 +0100

    Implemented user authentication system

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import register_view
+from .views import (
+    register_view,
+    login_view,
+    dashboard_view,
+    logout_view
+)
 
 urlpatterns = [
+    path('', login_view, name='login'),
     path('register/', register_view, name='register'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('logout/', logout_view, name='logout'),
 ]
--- user_management/views.py
+++ user_management/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
 from .forms import RegisterForm
 
 
@@ -7,10 +11,39 @@ def register_view(request):
         form = RegisterForm(request.POST)
 
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+
+            return redirect('dashboard')
 
     else:
         form = RegisterForm()
 
     return render(request, 'user_management/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            return redirect('dashboard')
+
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'user_management/login.html', {'form': form})
+
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'user_management/dashboard.html')
+
+
+def logout_view(request):
+    logout(request)
+
+    return redirect('login')
