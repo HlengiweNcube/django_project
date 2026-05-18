3a2ea0d6c89bcc30b7a6a9b0b26411977e3cf161
HlengiweNcube <hlehlencube123@gmail.com>
2026-05-18 10:10:40 +0100

    Implemented authentication and inventory app setup

--- django_final/settings.py
+++ django_final/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user_management',
+    'inventory',
+    'imports',
+    'exports',
+    'messaging',
 ]
 
 MIDDLEWARE = [
