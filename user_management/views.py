9f96c4b62ded40f28c83ca8e0d16e32b7df9982c
HlengiweNcube <hlehlencube123@gmail.com>
2026-05-25 19:37:29 +0100

    adding dashborad details

--- user_management/views.py
+++ user_management/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
+from inventory.models import Product
+from imports.models import ImportRecord
+from exports.models import ExportRecord
 
 from .forms import RegisterForm
 
@@ -40,7 +43,33 @@ def login_view(request):
 
 @login_required
 def dashboard_view(request):
-    return render(request, 'user_management/dashboard.html')
+
+    total_products = Product.objects.count()
+
+    total_imports = ImportRecord.objects.count()
+
+    total_exports = ExportRecord.objects.count()
+
+    low_stock_products = Product.objects.filter(
+        quantity__lt=10
+    )
+
+    context = {
+
+        'total_products': total_products,
+
+        'total_imports': total_imports,
+
+        'total_exports': total_exports,
+
+        'low_stock_products': low_stock_products,
+    }
+
+    return render(
+        request,
+        'user_management/dashboard.html',
+        context
+    )
 
 
 def logout_view(request):
